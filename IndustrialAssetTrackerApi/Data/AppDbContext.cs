1323d472b0105f10c7fb1fa9d080ed4ca94f813a
martavukovic <martavukovic0@gmail.com>
2026-03-28 18:46:02 +0100

    Add Entity Framework Core and AppDbContext for database connection

---
+++ IndustrialAssetTrackerApi/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IndustrialAssetTrackerApi.Models;
+
+namespace IndustrialAssetTrackerApi.Data
+{
+    public class AppDbContext (DbContextOptions <AppDbContext> options): DbContext(options)
+    {
+        public DbSet<Asset> Assets => Set<Asset>();  
+    }
+}
--- IndustrialAssetTrackerApi/Program.cs
+++ IndustrialAssetTrackerApi/Program.cs
@@ -1,4 +1,6 @@
+using IndustrialAssetTrackerApi.Data;
 using IndustrialAssetTrackerApi.Services;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 builder.Services.AddScoped<IAssetService, AssetService>();
 
